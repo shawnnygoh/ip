0a617f8aaf3315754909d1553788e340a18be7ae
shawnnygoh <shawnnygoh@gmail.com>
2025-01-18 18:08:09 +0800

    Rename Duke to Ally and implement greeting

---
+++ src/main/java/Ally.java
@@ -0,0 +1,21 @@
+public class Ally {
+    public static void main(String[] args) {
+        String logo = "    _    _ _       \n"
+                + "   / \\  | | |_   _ \n"
+                + "  / _ \\ | | | | | |\n"
+                + " / ___ \\| | | |_| |\n"
+                + "/_/   \\_\\_|_|\\__, |\n"
+                + "             |___/ \n";
+
+        String horizontalDivider = "____________________________________________________________";
+
+        System.out.println(horizontalDivider);
+        System.out.println(logo);
+        System.out.println("Hello! I'm Ally!");
+        System.out.println("What can I do for you?");
+        System.out.println(horizontalDivider);
+
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalDivider);
+    }
+}
